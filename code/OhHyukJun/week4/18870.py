4da761363e3b7233ad8381ff9e05702041afc376
OhHyukJun <kmh7277@naver.com>
2024-01-24 17:20:41 +0900

    Feat: 좌표 압축

--- code/OhHyukJun/week4/18870.py
+++ code/OhHyukJun/week4/18870.py
@@ -4,7 +4,8 @@ sort_arr = sorted(set(arr))
 
 sort_dict = {}
 for i in range(len(sort_arr)):
-    sort_dict[sort_arr[i]] = i #정렬했을 때의 인덱스 번호가 현재 자신보다 작은 정수의 수를 의미
+    sort_dict[sort_arr[i]] = i 
+    #정렬했을 때의 인덱스 번호가 현재 자신보다 작은 정수의 수를 의미
 
 for i in range(len(arr)):
     if arr[i] in sort_dict:
