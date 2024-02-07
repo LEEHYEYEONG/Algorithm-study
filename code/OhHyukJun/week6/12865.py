3e962089493849e15d4448204270fe6336c093a5
OhHyukJun <kmh7277@naver.com>
2024-02-07 17:41:15 +0900

    Feat: 12865.py

---
+++ code/OhHyukJun/week6/12865.py
@@ -0,0 +1,14 @@
+N, K = map(int, input().split())
+arr = []
+for _ in range(N):
+    W, V = map(int,input().split())
+    arr.append((W,V))
+
+arr.sort()
+dp = [0 for _ in range(K+1)]
+
+for W, V in arr:
+    for j in range(K, W-1, -1):
+        dp[j] = max(dp[j],dp[j-W]+V)
+
+print(max(dp))
